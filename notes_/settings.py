802de4662954732c69269a48af90703245d02ea3
Abhishek Vishwakarma <109618783+Abhi-vish@users.noreply.github.com>
2023-05-30 23:39:03 +0530

    Update settings.py

--- notes_/settings.py
+++ notes_/settings.py
@@ -12,8 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url
+import dj_database_url as dj
 from  decouple import config
+import django
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,17 +80,12 @@ WSGI_APPLICATION = 'notes_.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-   ''' 'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME':'railway',
-        'USER':'postgres',
-        'PASSOWRD':'containers-us-west-12.railway.app',
-        'HOST':'containers-us-west-12.railway.app',
-        'PORT':'7269'
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-    '''
 }
-DATABASES['default'] = dj_database_url.config()
+# DATABASES['default'] = dj.database_url.config()
 
 
 # Password validation
