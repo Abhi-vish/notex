e86b8fcd266060b34e79e1124c78feecf9e1a5f8
Abhishek Vishwakarma <109618783+Abhi-vish@users.noreply.github.com>
2023-06-13 12:49:22 +0530

    Update settings.py

--- notes_/settings.py
+++ notes_/settings.py
@@ -80,9 +80,9 @@ WSGI_APPLICATION = 'notes_.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 # DATABASES['default'] = dj.database_url.config()
